6e1bc0c98289f72cc8270c38567641e0cb359edf
Frank Bültge <frank@bueltge.de>
2011-06-07 00:23:33 +0200

    change js for lighter code, also style for work without classes

--- js/git.js
+++ js/git.js
@@ -8,17 +8,17 @@ jQuery(document).ready(function($) {
 
 			if (this.name != username + '.github.com') {
 				var repoDescription = this.fork ? ('<span class="forked">forked</span>') : this.description,
-				    line = $('<li style="" class="repo"> \
-						<h3 class="repo-title"><a href="' + this.url + '">' + this.name + '</a></h3> \
-						<span class="pushed_at" title="Pushed at">' + this.pushed_at.slice(0, 10) + '</span> \
+				    line = $('<li> \
+						<h3><a href="' + this.url + '">' + this.name + '</a></h3> \
+						<span title="Pushed at">' + this.pushed_at.slice(0, 10) + '</span> \
 						<div> \
 							<span>' + this.language + '</span> \
-							<span class="watchers" title="Watchers">' + this.watchers + '</span> \
-							<span class="forks" title="Forks">' + this.forks + '</span> \
+							<span title="Watchers">' + this.watchers + '</span> \
+							<span title="Forks">' + this.forks + '</span> \
 						</div> \
 						<p> \
 							' + repoDescription + ' \
-							<a class="homepage" title="Link to Homepage or Post" href="' + this.homepage + '">Homepage or Post</a> \
+							<a title="Link to Homepage or Post" href="' + this.homepage + '">Homepage or Post</a> \
 						</p> \
 					</li>').hide();
 
