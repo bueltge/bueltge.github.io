365d9f3c8296ab6f7ed858521531e3cdbfbd532f
fhemberger <mail@frederic-hemberger.de>
2011-06-06 23:39:53 +0200

    Replaced manual DOM node creation with HTML string handeled by jQuery

--- js/gist.js
+++ js/gist.js
@@ -1,41 +1,23 @@
-jQuery(document).ready( function($) {
+jQuery(document).ready(function($) {
 	
 	var repos = $('#gists');
 	var username = 'bueltge';
 
 	$.getJSON('https://gist.github.com/api/v1/json/gists/' + username + '?callback=?', function(data, status) {
 		$.each(data.gists, function() {
-			var created_at = document.createElement('span');
-			$(created_at).html( this.created_at.slice(0, 10) ).attr('class', 'created_at').attr('title', 'Created at');
-		
-			var description = document.createElement('p');
-			$(description).html(this.description);
-		
-			if ('' == this.description) {
-				$(description).html('<em>no description</em>');
-			}
-		
-			var repolink = document.createElement('a');
-			$(repolink).attr('href', 'https://gist.github.com/'+this.repo);
-			$(repolink).html('gist: '+this.repo);
-		
-			var title = document.createElement('h3');
-			$(title).append(repolink).attr('class', 'repo-title');
-		
-			if ( '' != this.comments ) {
-				var comments = document.createElement('span');
-				$(comments).html( '!' ).attr('class', 'comments').attr('title', 'There are comments');
-			}
-		
-			var text = document.createElement('p');
-			var line = document.createElement('li');
-			var more = document.createElement('div');
-			if ('' != this.comments)
-				$(more).append(comments);
-			$(line).hide().append(title).append(created_at).append(more).append(description).attr('class', 'repo');
-		
+			
+			var repoDescription = (this.description !== '') ? this.description : '<em>no description</em>',
+			    repoComments = (this.comments.length !== 0) ? '<span class="comments" title="There are comments">!</span>' : '',
+			    line = $('<li class="repo"> \
+					<h3 class="repo-title"><a href="https://gist.github.com/' + this.repo + '">gist: ' + this.repo + '</a></h3> \
+					<span class="created_at" title="Created at">' + this.created_at.slice(0, 10) + '</span> \
+					<div>' + repoComments + '</div> \
+					<p>' + repoDescription + '</p> \
+				</li>').hide();
+
 			$(repos).append(line);
-			$(line).fadeIn(700);
+			$(line).fadeIn(500);
+
 		});
 	});
 });
--- js/git.js
+++ js/git.js
@@ -1,54 +1,31 @@
-jQuery(document).ready( function($) {
+jQuery(document).ready(function($) {
 
 	var repos = $('#repositories');
 	var username = 'bueltge';
 
 	$.getJSON('http://github.com/api/v2/json/repos/show/' + username + '?callback=?', function(data, status) {
 		$.each(data.repositories.reverse(), function() {
-			var repolink = document.createElement('a');
-			$(repolink).attr('href', this.url);
-			$(repolink).html(this.name);
-			
-			var title = document.createElement('h3');
-			$(title).append(repolink).attr('class', 'repo-title');
 
-			var language = document.createElement('span');
-			$(language).html(this.language);
-			
-			if ( this.homepage ) {
-				var homepage = document.createElement('a');
-				$(homepage).attr('class', 'homepage');
-				$(homepage).attr('title', 'Link to Homepage or Post');
-				$(homepage).attr('href', this.homepage);
-				$(homepage).html('Homepage or Post');
-			}
-			
-			var forkbatch = document.createElement('span');
-			$(forkbatch).attr('class', 'forked').text('forked');
-			
-			var watchers = document.createElement('span');
-			$(watchers).html(this.watchers).attr('class', 'watchers').attr('title', 'Watchers');
-			
-			var forks = document.createElement('span');
-			$(forks).html(this.forks).attr('class', 'forks').attr('title', 'Forks');
-			
-			var pushed_at = document.createElement('span');
-			$(pushed_at).html( this.pushed_at.slice(0, 10) ).attr('class', 'pushed_at').attr('title', 'Pushed at');
-			
-			var text = document.createElement('p');
-			var line = document.createElement('li');
-			var more = document.createElement('div');
-			if (this.language)
-				$(more).append(language);
-			$(more).append(watchers).append(forks);
-			$(line).hide().append(title).append(pushed_at).append(more).append(text).attr('class', 'repo');
-			this.fork ? $(line).append(forkbatch) : $(text).html(this.description).append(homepage);
-			
 			if (this.name != username + '.github.com') {
+				var repoDescription = this.fork ? ('<span class="forked">forked</span>') : this.description,
+				    line = $('<li style="" class="repo"> \
+						<h3 class="repo-title"><a href="' + this.url + '">' + this.name + '</a></h3> \
+						<span class="pushed_at" title="Pushed at">' + this.pushed_at.slice(0, 10) + '</span> \
+						<div> \
+							<span>' + this.language + '</span> \
+							<span class="watchers" title="Watchers">' + this.watchers + '</span> \
+							<span class="forks" title="Forks">' + this.forks + '</span> \
+						</div> \
+						<p> \
+							' + repoDescription + ' \
+							<a class="homepage" title="Link to Homepage or Post" href="' + this.homepage + '">Homepage or Post</a> \
+						</p> \
+					</li>').hide();
+
 				$(repos).append(line);
+				$(line).fadeIn(500);
 			}
-			$(line).fadeIn(500);
+
 		});
 	});
-
 });
