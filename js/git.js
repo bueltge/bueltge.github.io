540c9987f4048309886c09242bb4eb672af6b94d
Frank Bültge <frank@bueltge.de>
2012-07-02 12:17:28 +0200

    Fix url to the project on github and remove this page project

--- js/git.js
+++ js/git.js
@@ -9,13 +9,13 @@ jQuery(document).ready( function($) {
 		
 		$.each(data.data, function(i, value) {
 			
-			if ( this.html_url != username + '.github.com' ) {
+			if ( this.homepage != username + '.github.com' ) {
 				console.dir(this);
 				var fork = this.fork ? ('<span class="forked">forked</span>') : (''),
 					open_issues = this.open_issues ?  ('<span title="Open Issues">I ' + this.open_issues + '</span>') : (''),
 					page = this.homepage ? ('<a title="Link to Homepage or Post" href="' + this.homepage + '">Homepage or Post</a>') : ('')
 					line = $('<li> \
-						<h3><a href="' + this.url + '">' + this.name + '</a></h3> \
+						<h3><a href="' + this.html_url + '">' + this.name + '</a></h3> \
 						' + fork + ' \
 						<span title="Pushed at">' + this.pushed_at.slice(0, 10) + '</span> \
 						<div> \
